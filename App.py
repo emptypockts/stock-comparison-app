2b0b10da9ef5d60cc1f15467dfeb3a80aa5af17f
jj <jjmr86@live.com.mx>
2024-09-27 13:07:45 -1000

    adjust folder structure taking out py files and keeping only auth and register in

--- App.py
+++ App.py
@@ -13,8 +13,8 @@ import os
 import bcrypt
 import jwt
 import datetime
-from authLogin import loginStep
-from authRegister import registerStep
+from api.authLogin import loginStep
+from api.authRegister import registerStep
 from datetime import timedelta
 load_dotenv()
 app = Flask(__name__)
