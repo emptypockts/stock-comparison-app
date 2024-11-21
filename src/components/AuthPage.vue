f63ae010b4c5dca2358e7f7c656813ae35fc9c3e
jj <jjmr86@live.com.mx>
2024-11-20 20:22:01 -1000

    removed outage message

--- src/components/AuthPage.vue
+++ src/components/AuthPage.vue
@@ -2,9 +2,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <div v-if="!loading" class="loading-overlay">
     <div class="loading-throbber">
-      <!-- <div class="spinner"></div> -->
-      <!-- <p>Authenticating... Please wait. DB powered by MongoDB</p> -->
-      <p>Outage with the finance API. Migrating API to another free platform  Apologies for the inconvenience</p>
+      <div class="spinner"></div>
+      <p>Authenticating... Please wait. DB powered by MongoDB</p>
+      <!-- <p>Outage with the finance API. Migrating API to another free platform  Apologies for the inconvenience</p> -->
     </div>
   </div>
   <div class="bg-image">
