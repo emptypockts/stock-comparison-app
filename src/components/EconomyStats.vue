da48f8a9bc90bbf87e4e64cd15cb632bfbe5f634
emptypockts <jjmr86@live.com.mx>
2025-01-05 19:00:22 -1000

    fixed spelling error in economy app

--- src/components/EconomyStats.vue
+++ src/components/EconomyStats.vue
@@ -22,7 +22,7 @@
       <div v-if="loading" class="loading-overlay">
         <div class="loading-throbber">
           <div class="spinner"></div>
-          <p1>Getting eonomy data...</p1>
+          <p1>Getting economy data...</p1>
         </div>
       </div>
     </div>
