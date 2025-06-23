335225754ffbfd5736d21445e760b66481b19f3c
emptypockts <jjmr86@live.com.mx>
2025-06-22 14:05:14 -1000

    testing sso with cloudflare

--- authLogin.py
+++ authLogin.py
@@ -2,6 +2,8 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import os
+import requests
+import jwt
 
 def loginStep(username):
     load_dotenv()
@@ -16,14 +18,24 @@ def loginStep(username):
     return user
 
 
-if __name__ == "__main__":
-    load_dotenv()
-    uri = os.getenv('MONGODB_URI')
-    client = MongoClient(uri, server_api=ServerApi('1'))
-    
-    try:
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
 
-    except Exception as e:
-        print(e)
+
+if __name__ == "__main__":
+    # load_dotenv()
+    from flask import Flask,request
+    app = Flask(__name__)
+    CF_CERT_URL = f"https://{os.getenv('CF_URL_CDN_CGI_CERTS')}/cdn-cgi/access/certs"
+    CERT_KYS = requests.get(CF_CERT_URL).json()
+    CF_AUDIENCE_ID = os.getenv('CF_AUD_ID')
+
+    # uri = os.getenv('MONGODB_URI')
+    # client = MongoClient(uri, server_api=ServerApi('1'))
+    
+    # try:
+    #     client.admin.command('ping')
+    #     print("Pinged your deployment. You successfully connected to MongoDB!")
+
+    # except Exception as e:
+    #     print(e)
+    token = request.headers.get("Cf-Access-Jwt-Assertion") or request.cookies.get("CF_Authorization")    
+    print(token)
