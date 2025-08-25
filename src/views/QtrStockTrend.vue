bfd30a439c5b799d00dabbd3c76c85a4857a386e
emptypockts <jjmr86@live.com.mx>
2025-08-24 14:49:39 -1000

    fixed total_score key of qtr chart data

--- src/views/QtrStockTrend.vue
+++ src/views/QtrStockTrend.vue
@@ -18,7 +18,7 @@
                 <th @click="sortTable('ticker')">Symbol<span class="sortClass"></span></th>
                 <th @click="sortTable('value')">Value in $ Billions <span class="sortClass"></span></th>
                 <th @click="sortTable('trend')">last year q comparison in %<span class="sortClass"></span></th>
-                <th @click="sortTable('score')">last 5 years stock score<span class="sortClass"></span></th>
+                <th @click="sortTable('total_score')">last 5 years stock score<span class="sortClass"></span></th>
               </tr>
             </thead>
             <tbody>
@@ -26,7 +26,7 @@
                 <td>{{ record['ticker'] }}</td>
                 <td>{{ record['value'] }}</td>
                 <td>{{ record['trend'] }}%</td>
-                <td>{{ record['score'] }}</td>
+                <td>{{ record['total_score'] }}</td>
               </tr>
             </tbody>
           </table>
