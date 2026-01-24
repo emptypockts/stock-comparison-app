fe9e4fc0203c9402c50d42a027411fa1d01f04b7
emptypockts <jjmr86@live.com.mx>
2026-01-24 11:47:38 -1000

    added walletconnect for ios users

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,30 @@
 import { createConfig,http } from '@wagmi/vue'
 import { hederaMainnet, hederaTestnet } from './HederaChain'
+import {metaMask,walletConnect} from 'wagmi/connectors'
 
+const projectId= import.meta.env.VITE_WALLETCONNECT_PROJECT_ID
+if (!projectId){
+  throw new Error('missing walletconnect project id')
+  
+}
 export const config = createConfig({
-  ssr: true,
   chains: [hederaTestnet,hederaMainnet],
   transports: {
     [hederaTestnet.id]: http(),
     [hederaMainnet.id]:http(),
   },
+  connectors:[
+    metaMask(),
+    walletConnect({
+      projectId:projectId,
+      metadata:{
+        name:'hbar donations',
+        description: 'donate using hbar',
+        url:window.location.origin,
+        icons:[`${window.location.origin}/assets/dahoncho.png`]
+      }
+    })
+  ]
 })
 
 declare module '@wagmi/vue' {
