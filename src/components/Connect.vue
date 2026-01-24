be43860e8c7c40b9b238655b161d2b5f90933d68
emptypockts <jjmr86@live.com.mx>
2026-01-24 12:09:42 -1000

    added logic to component to switch between metamask and walletconnect

--- src/components/Connect.vue
+++ src/components/Connect.vue
@@ -1,20 +1,31 @@
 <template>
   <span>eacsa> </span>
   connect wallet
-  <button v-for="connector in connectors" :key="connector.id" type="button" @click="connect.mutate({connector, chainId})" class="buttons">
+  <button v-for="connector in connectors" 
+  :key="connector.id" 
+  type="button" 
+  @click="connect.mutate({connector, chainId})" 
+  class="buttons">
     {{ connector.name}}
   </button>
 </template>
 
+
 <script setup lang="ts">
+
 import { useChainId, useConnect, useConnectors } from '@wagmi/vue'
 import { computed } from 'vue';
+const isIos= /iPad|iPhone|iPod/.test(navigator.userAgent)
 const allConnectors=useConnectors()
 const chainId = useChainId()
 const connect = useConnect()
 const connectors = computed(()=>
-allConnectors.value.filter(
-  (c)=> c.name==='MetaMask'
+allConnectors.value.filter((c)=>{ 
+  if (isIos){
+    return c.id==='walletConnect'
+  }
+  return c.id==='metaMaskSDK'
+}
 )
 )
 
